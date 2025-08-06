b960a65c3249f9a69f5c7f39ecfb68c71f8a9c22
UrbanPistek <urban.pistek@gmail.com>
2025-08-06 00:31:57 -0600

    WIP - saving

--- ml-onnx/scraper.py
+++ ml-onnx/scraper.py
@@ -5,7 +5,9 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from bs4 import BeautifulSoup
 from rapidfuzz import fuzz
+from adjustText import adjust_text
 
+URL = "https://docs.pytorch.org/vision/main/models.html#table-of-all-available-classification-weights"
 
 def get_data_table_from_url(url: str) -> pd.DataFrame:
 
@@ -31,31 +33,28 @@ def get_data_table_from_url(url: str) -> pd.DataFrame:
     return df
 
 
-def plot_models_table(df: pd.DataFrame):
+def plot_models_table(df: pd.DataFrame, xcol: str, ycol: str, label: str):
 
     # Set seaborn theme
     sns.set_theme(style="whitegrid", context="talk")
 
     # Create the scatterplot
     plt.figure(figsize=(16, 9))
-    scatter = sns.scatterplot(data=df, x='Params', y='Acc@1', s=60, color='dodgerblue', edgecolor='black', alpha=0.7)
+    scatter = sns.scatterplot(data=df, x=xcol, y=ycol, s=60, color='dodgerblue', edgecolor='black', alpha=0.7)
 
     # Annotate a subset of points to avoid clutter
-    top_annotate = df.nlargest(5, 'Acc@1')
-    bottom_annotate = df.nsmallest(5, 'Acc@1')
-    r_annotate = df.nsmallest(5, 'acc_per_params_M')
+    annotate = df[df['Acc@1'] > 85]
+    texts = []
+    for i, row in annotate.iterrows():
+        texts.append(plt.text(row[xcol], row[ycol], row[label], fontsize=9, weight='bold', alpha=0.8))
 
-    for _, row in top_annotate.iterrows():
-        plt.text(row['Params'], row['Acc@1'], row['Weight'], fontsize=9, weight='bold', alpha=0.8)
-    for _, row in bottom_annotate.iterrows():
-        plt.text(row['Params'], row['Acc@1'], row['Weight'], fontsize=9, weight='bold', alpha=0.8)
-    for _, row in r_annotate.iterrows():
-        plt.text(row['Params'], row['Acc@1'], row['Weight'], fontsize=9, weight='bold', alpha=0.8)
+    # Automatically adjust text to reduce overlap
+    adjust_text(texts, arrowprops=dict(arrowstyle='->', color='gray', lw=0.5))
 
     # Improve layout
-    plt.title('Model Accuracy vs Parameters', fontsize=16)
-    plt.xlabel('Params (millions)', fontsize=14)
-    plt.ylabel('Top-1 Accuracy (%)', fontsize=14)
+    plt.title(f'{ycol} vs {xcol}', fontsize=16)
+    plt.xlabel(xcol, fontsize=14)
+    plt.ylabel(ycol, fontsize=14)
     plt.xticks(rotation=45)
     plt.tight_layout()
     plt.show()
@@ -69,19 +68,26 @@ def replace_similar_substrings(text: str, target: str, threshold=80):
     ]
     return ' '.join(new_words)
 
-url = "https://docs.pytorch.org/vision/main/models.html#table-of-all-available-classification-weights"
-df = get_data_table_from_url(url)
-print(df.head())
 
-#
-# df['Weight'] = df['Weight'].str.replace('_Weights.IMAGENET1K_V1', '', regex=False)
-df['Weight'] = df['Weight'].apply(lambda x: replace_similar_substrings(x, "IMAGENET1K_V1", 50))
-df['Weight'] = df['Weight'].str.replace('_Weights', '', regex=False)
+def main(url: str):
 
-# Get acc / params ratio
-df['Params'] = df['Params'].str.replace('M', '', regex=False).astype(float) # remove M and convert to float
-df["acc_per_params_M"] = df["Acc@1"] / df["Params"]
-top5 = df.nlargest(5, 'acc_per_params_M')
-print(top5)
+    df = get_data_table_from_url(url)
+    print(df.head())
 
-plot_models_table(df)
+    # Clean up names 
+    df['Weight'] = df['Weight'].apply(lambda x: replace_similar_substrings(x, "IMAGENET1K_V1", 50))
+    df['Weight'] = df['Weight'].str.replace('_Weights', '', regex=False)
+
+    # Get acc / params ratio
+    df['Params'] = df['Params'].str.replace('M', '', regex=False).astype(float) # remove M and convert to float
+    df["acc_per_params_M"] = df["Acc@1"] / df["Params"]
+    top5 = df.nlargest(5, 'acc_per_params_M')
+    print(top5)
+
+    plot_models_table(df, xcol='Params', ycol='Acc@1', label='Weight')
+    plot_models_table(df, xcol='GFLOPS', ycol='Acc@1', label='Weight')
+
+    # Save
+    df.to_csv('data/pytorch_models_data.csv')
+
+main(URL)
--- ml-onnx/tune.py
+++ ml-onnx/tune.py
@@ -5,12 +5,27 @@ import torch.optim as optim
 import numpy as np
 from torch.utils.data import DataLoader, random_split
 from torchvision import datasets, transforms, models
-from torchvision.models import RegNet_X_32GF_Weights
+from torchvision.models import RegNet_Y_16GF_Weights, EfficientNet_V2_M_Weights
 from torchinfo import summary
 
-model = models.regnet_x_32gf(weights=RegNet_X_32GF_Weights.IMAGENET1K_V2)
-summary(model, input_size=(1, 3, 256, 256), depth=3)
 
-numFeatures = model.fc.in_features
-numClasses = 75
-model.fc = nn.Linear(numFeatures, numClasses)
+def load_model(numClasses):
+    model = models.regnet_y_16gf(weights=RegNet_Y_16GF_Weights.IMAGENET1K_V2)
+    numFeatures = model.fc.in_features
+    model.fc = nn.Linear(numFeatures, numClasses)
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.parameters(), lr=0.0001)
+
+    summary(model, input_size=(1, 3, 256, 256), depth=3)
+    return model, criterion, optimizer
+
+
+def load_data(path: str):
+    pass
+
+
+def main():
+    
+    # Load model
+    numClasses = 75
+    model, criterion, optimizer = load_model(numClasses)
