63087c295f9f5be97f3d589faf222e92221170fa
UrbanPistek <urban.pistek@gmail.com>
2025-09-19 08:38:41 -0600

    Added updated model types plot

--- ml-onnx/articles/a1_tuning_and_onnx.md
+++ ml-onnx/articles/a1_tuning_and_onnx.md
@@ -1,2 +1,42 @@
-# Tuning Models & Using ONNX for Inference in Python & Rust
+# Fine Tuning Models in Pytorch & Using ONNX for Inference in Python & Rust
 
+## [1] Overview
+
+I want to mostly focus on the Inference side of things and how ONNX can be leveraged, but to get there I want to cover fine tuning a model to serve as a starting point and as a working example. Then I will dive into the specifics of how to use ONNX for inference in both rust and python, also covering how some of the pre-processing transforms can be replicated without having to rely on the transforms used in pytorch. 
+
+### Dataset
+
+Dataset: [Butterfly Image Classification](https://www.kaggle.com/datasets/phucthaiv02/butterfly-image-classification)
+
+I decided to use this image dataset because I thought it was a little more interesting and wanted to work with image based data for this example. If you wish to follow along, download it from the link above.
+
+### Model Tuning & Selection
+
+So why tuning a existing model here? The biggest reasons is efficiency - both in terms of cost and time in addition to getting faster convergence. By taking a more general existing model its weights are already adjusted to a point where it has a strong foundational knowledge in that general task. So tuning it requires much less training time to have adjust to your specific task. There are cases where training from sratch makes more sense, however I do think in a lot of senarios fine tuning just makes a lot of sense, and can give you great results with much less effort - overall more efficient. 
+
+> [Introduction to Fine-Tuning in Machine Learning](https://www.oracle.com/ca-en/artificial-intelligence/fine-tuning/)
+
+So we've selected a image dataset to use for this example and the general problem we are trying to address here is a classification task. So we'll want to select a image classification foundation model to use for our base, from [PyTorch Vision](https://docs.pytorch.org/vision/main/models.html), there are quite a few models to select from for this problem. Naturally, from the table there are so many models to choose from I was not sure what I wanted to select - model selection can be very nuanced but for this sample, I wanted to use a rough critieria to select something decent. 
+
+I started by using some basic web-scraping to pull the table for image classification pre-trained models from [here](https://docs.pytorch.org/vision/main/models.html#table-of-all-available-classification-weights) - if you wish to see how I did this, see this script: `scripts/models_data_scraper.py`. You can also get the data [here](./media/pytorch_models_data.csv) if you wish to do some analysis yourself.
+
+So, I made two graphs:
+
+![Acc vs Params](./media/Params_(M)_v_Acc@1.png)
+
+![Acc vs GFLOPS](./media/GFLOPS_v_Acc@1.png)
+
+## [2] Model Tuning
+
+## [3] Exporting to ONNX
+
+## [4] ONNX Inference in Python
+
+## [5] ONNX Inference in Rust
+
+## [6] Summary
+
+## [7] References
+
+1. https://www.kaggle.com/datasets/phucthaiv02/butterfly-image-classification
+2. https://www.oracle.com/ca-en/artificial-intelligence/fine-tuning/
--- ml-onnx/scripts/models_data_scraper.py
+++ ml-onnx/scripts/models_data_scraper.py
@@ -34,32 +34,85 @@ def get_data_table_from_url(url: str) -> pd.DataFrame:
     return df
 
 
-def plot_models_table(df: pd.DataFrame, xcol: str, ycol: str, label: str):
+def plot_models_table(
+        df: pd.DataFrame, 
+        xcol: str, 
+        ycol: str, 
+        label: str,
+        title: str, 
+        xaxis: str,
+        yaxis: str
+    ):
 
     # Set seaborn theme
     sns.set_theme(style="whitegrid", context="talk")
 
     # Create the scatterplot
     plt.figure(figsize=(16, 9))
-    scatter = sns.scatterplot(data=df, x=xcol, y=ycol, s=60, color='dodgerblue', edgecolor='black', alpha=0.7)
 
     # Annotate a subset of points to avoid clutter
-    annotate = df[df['Acc@1'] > 85]
+    annotate_acc = df[df['Acc@1'] > 85]
+
+    # Get unique names
+    unique_names = df['model_type'].unique()
+    print("Unique model types:", unique_names)
+
+    # Get rows with highest and lowest scores for each name
+    def get_min_max_rows(group):
+        max_idx = group['Acc@1'].idxmax()
+        min_idx = group['Acc@1'].idxmin()
+        
+        # If min and max are the same (only one row), return just one row
+        if max_idx == min_idx:
+            return group.loc[[max_idx]]
+        else:
+            return group.loc[[max_idx, min_idx]]
+
+    result = (
+        df.groupby('model_type', group_keys=False)
+        .apply(get_min_max_rows, include_groups=False)
+        .reset_index(drop=True)
+    )
+
+    # Add scatter hue based on model types
+    palette = sns.color_palette("tab20", n_colors=len(unique_names))
+    scatter = sns.scatterplot(data=df, x=xcol, y=ycol, s=60, hue="model_type", palette=palette, edgecolor='black', alpha=0.7)
+    
+    # Keep only rows unique to result
+    merged = result.merge(annotate_acc, how="left", indicator=True)
+    annotate = merged[merged["_merge"] == "left_only"].drop(columns="_merge")
+
+    # Create 2 sets of texts to emphasize certain features more in 1 set
     texts = []
     for i, row in annotate.iterrows():
-        texts.append(plt.text(row[xcol], row[ycol], row[label], fontsize=9, weight='bold', alpha=0.8))
+        texts.append(plt.text(row[xcol], row[ycol], row[label], fontsize=8, weight='normal', alpha=1))
+    for i, row in annotate_acc.iterrows():
+        texts.append(plt.text(row[xcol], row[ycol], row[label], fontsize=8, weight='bold', alpha=1))
 
     # Automatically adjust text to reduce overlap
-    adjust_text(texts, arrowprops=dict(arrowstyle='->', color='gray', lw=0.5))
+    adjust_text(
+        texts, 
+        expand_points=(2.0, 2.0),  # expand distance from points (x, y)
+        expand_text=(1.5, 1.5),    # expand distance from other texts
+        force_text=(0.5, 0.5),     # strength of repulsion between texts
+        arrowprops=dict(arrowstyle='-', color='gray', lw=0.75), 
+        iter_lim=1000
+    )
 
     # Improve layout
-    plt.title(f'{ycol} vs {xcol}', fontsize=16)
-    plt.xlabel(xcol, fontsize=14)
-    plt.ylabel(ycol, fontsize=14)
+    plt.title(title, fontsize=16)
+    plt.xlabel(xaxis, fontsize=14)
+    plt.ylabel(yaxis, fontsize=14)
     plt.xticks(rotation=45)
-    plt.tight_layout()
+    plt.legend(
+        title="Model Groups", 
+        ncol=3, # number of columns in the legend
+        frameon=True,
+        fontsize=8, 
+        title_fontsize=10
+    )
+    plt.savefig(f"{title.replace(' ', '_')}", dpi=300, bbox_inches='tight')
     plt.show()
-
 
 def replace_similar_substrings(text: str, target: str, threshold=80):
     words = text.split(".")
@@ -85,8 +138,11 @@ def main(url: str):
     top5 = df.nlargest(5, 'acc_per_params_M')
     print(top5)
 
-    plot_models_table(df, xcol='Params', ycol='Acc@1', label='Weight')
-    plot_models_table(df, xcol='GFLOPS', ycol='Acc@1', label='Weight')
+    # Extract the name part before the underscore
+    df['model_type'] = df['Weight'].str.split('_').str[0].str.replace(r'\d+', '', regex=True).str.strip()
+
+    plot_models_table(df, xcol='Params', ycol='Acc@1', label='Weight', title='Params (M) v Acc@1', xaxis='Params (M)', yaxis='Acc@1')
+    plot_models_table(df, xcol='GFLOPS', ycol='Acc@1', label='Weight', title='GFLOPS v Acc@1', xaxis='GFLOPS', yaxis='Acc@1')
 
     # Save
     if not os.path.exists("../data"):
